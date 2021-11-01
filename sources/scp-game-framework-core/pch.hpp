647cbcc712bb01de4076f6d9bc2093aee1ca320c
Liangyue Zhao <liangyuez1@outlook.com>
2021-11-01 16:46:49 -0400

    Added a precompiled header.

---
+++ sources/scp-game-framework-core/pch.hpp
@@ -0,0 +1,8 @@
+#ifndef FD473812_6F6E_48FC_8EED_8CB55403A5AA
+#define FD473812_6F6E_48FC_8EED_8CB55403A5AA
+
+#include <iostream>
+#include <vector>
+#include <memory>
+
+#endif /* FD473812_6F6E_48FC_8EED_8CB55403A5AA */
