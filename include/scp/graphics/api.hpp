6273c96107b506ab56cede8f162a20b9869e2783
Liangyue Zhao <liangyuez1@outlook.com>
2021-11-01 16:50:47 -0400

    Added the API enumeration.

---
+++ include/scp/graphics/api.hpp
@@ -0,0 +1,18 @@
+#ifndef A5E1BBBD_398C_43EE_9671_837D9AC3FBF4
+#define A5E1BBBD_398C_43EE_9671_837D9AC3FBF4
+
+namespace scp::graphics
+{
+    // This enumeration represents the graphics API the framework is currently
+    // using. It mainly applies to window creation, as this framework does not
+    // yet have an API agnostic wrapper.
+    //
+    // This is also the full list of all of the supported APIs.
+    enum class api
+    {
+        opengl,
+        vulkan
+    }
+}
+
+#endif /* A5E1BBBD_398C_43EE_9671_837D9AC3FBF4 */
