6826c76b49e875d4f572b91b3d0fb8cb41609b30
Liangyue Zhao <liangyuez1@outlook.com>
2021-11-02 19:02:17 -0400

    Finished the implementation of the window class

--- sources/scp-game-framework-core/window.cpp
+++ sources/scp-game-framework-core/window.cpp
@@ -1,8 +1,13 @@
 #include "pch.hpp"
 
+#include <glad/glad.h>
+
 #include <scp/window.hpp>
 
 using scp::window;
+
+// The error callback for glfw
+static void error_callback(int error_code, const char* message);
 
 window& window::get_instance()
 {
@@ -83,8 +88,75 @@ window::window(uint32_t p_width, uint32_t p_height, std::string_view p_title, bo
 {
     if (!glfwInit())
     {
-        // TODO: Throw an exception
+        throw std::runtime_error("Failed to initialize GLFW.");
     }
     
+    glfwSetErrorCallback(error_callback);
     
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    
+    if (p_graphics_api == scp::graphics::api::opengl)
+    {
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    } else
+    {
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    }
+    
+    GLFWmonitor* monitor = nullptr;
+    const GLFWvidmode* video_mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    
+    if (p_fullscreen)
+    {
+        monitor = glfwGetPrimaryMonitor();
+        m_data.m_width = video_mode->width;
+        m_data.m_height = video_mode->height;
+    } else
+    {
+        m_data.m_width = p_width;
+        m_data.m_height = p_height;
+    }
+    
+    m_handle = glfwCreateWindow(m_data.m_width, m_data.m_height, p_title.data(), monitor, nullptr);
+    if (m_handle == nullptr)
+    {
+        throw std::runtime_error("Failed to create the GLFW window.");
+    }
+    
+    glfwSetWindowUserPointer(m_handle, &m_data);
+    
+    if (p_graphics_api == scp::graphics::api::opengl)
+    {
+        glfwMakeContextCurrent(m_handle);
+        
+        if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+        {
+            // TODO: Throw a custom exception to specify that glad OpenGL
+            // has failed.
+        }
+        
+        glfwSetFramebufferSizeCallback(m_handle, opengl_window_resize_callback);
+    }
+}
+
+
+
+
+static void error_callback(int p_error_code, const char* p_message)
+{
+    std::cerr << "[GLFW ERROR " << p_error_code << "]: " << p_message << std::endl;
+}
+
+
+
+
+void window::opengl_window_resize_callback(GLFWwindow* p_window, int p_width, int p_height)
+{
+    window_data* data = reinterpret_cast<window_data*>(glfwGetWindowUserPointer(p_window));
+    data->m_width = p_width;
+    data->m_height = p_height;
+    
+    glViewport(0, 0, data->m_width, data->m_height);
 }
