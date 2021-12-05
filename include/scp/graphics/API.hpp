7aaa1eb748bcf69f20d8d2522c93db86c4521829
Liangyue "Yue" Zhao <Hello56721@github.com>
2021-12-05 10:39:26 -0500

    Added an enumeration to represent the graphics API.

---
+++ include/scp/graphics/API.hpp
@@ -0,0 +1,12 @@
+#ifndef A35F0D22_56E4_4F86_82CF_11D129B9BAFE
+#define A35F0D22_56E4_4F86_82CF_11D129B9BAFE
+
+namespace scp::graphics
+{
+    enum class API
+    {
+        OpenGL, Vulkan, Direct3D, Direct3D_12, Metal
+    }
+}
+
+#endif /* A35F0D22_56E4_4F86_82CF_11D129B9BAFE */
