4819d52c6f6131ac3942e5e7e8006a4db29c7bca
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2021-12-09 17:25:22 -0500

    Fixed a crash that occurs due to the new window event system

--- tests/basic-window-test/basic-window-test.cpp
+++ tests/basic-window-test/basic-window-test.cpp
@@ -1,12 +1,16 @@
 #include <scp/Window.hpp>
+#include <scp/Scene.hpp>
 
 using scp::Window;
+using scp::Scene;
 
 int main()
 {
     std::cout << "[INFO]: Hello!" << std::endl;
     
     Window window(Window::SCREEN_SIZE_TIMES_66_PERCENT, Window::SCREEN_SIZE_TIMES_66_PERCENT, "Basic Window Test");
+    
+    Scene::setActive<Scene>();
     
     window.show();
     
