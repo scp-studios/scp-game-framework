9b18342f1348ff1b14ff505435106c4bceaba711
Liangyue "Yue" Zhao <Hello56721@github.com>
2021-12-17 10:43:44 -0500

    Added a utility function to see if a string starts with another string.

---
+++ include/scp/utils/string_utils.hpp
@@ -0,0 +1,14 @@
+#ifndef C38DD9E2_0700_478E_AFFE_836CEF804595
+#define C38DD9E2_0700_478E_AFFE_836CEF804595
+
+#include "pch.hpp"
+
+namespace scp::utils
+{
+    namespace string_utils
+    {
+        bool startsWith(std::string_view a, std::string_view b);
+    }
+}
+
+#endif /* C38DD9E2_0700_478E_AFFE_836CEF804595 */
---
+++ src/utils/string_utils.cpp
@@ -0,0 +1,16 @@
+#include <scp/utils/string_utils.hpp>
+
+namespace string_utils = scp::utils::string_utils;
+
+bool string_utils::startsWith(std::string_view p_a, std::string_view p_b)
+{
+    for (std::string_view::iterator aIt = p_a.begin(), bIt = p_b.begin(); bIt != p_b.end(); aIt++, bIt++)
+    {
+        if (*aIt != *bIt)
+        {
+            return false;
+        }
+    }
+    
+    return false;
+}
