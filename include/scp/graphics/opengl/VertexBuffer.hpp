c182b56e7cc3a652948b88bf548d9b7eaf9aa967
Liangyue "Yue" Zhao <Hello56721@github.com>
2021-12-19 18:07:40 -0500

    Started on the vertex buffer class.

---
+++ include/scp/graphics/opengl/VertexBuffer.hpp
@@ -0,0 +1,59 @@
+#ifndef CD895C87_5211_435E_820F_54EDC3B94158
+#define CD895C87_5211_435E_820F_54EDC3B94158
+
+#include <scp/graphics/pch.hpp>
+
+namespace scp::graphics::opengl
+{
+    class VertexBuffer
+    {
+    public:
+        VertexBuffer() = default;
+        
+        // Construct an empty vertex buffer with the specified size
+        VertexBuffer(uint32_t size);
+        
+        // Construct a vertex buffer from an arbitrary vector
+        template<typename T>
+        VertexBuffer(std::vector<T> p_data)
+        {
+            glGenBuffers(1, &m_handle);
+            glBindBuffer(GL_ARRAY_BUFFER, m_handle);
+            glBufferData(GL_ARRAY_BUFFER, p_data.size() * sizeof(T), p_data.data(), GL_STATIC_DRAW); // TODO: Might add a little optional thingy here
+            
+            glBindBuffer(GL_ARRAY_BUFFER, 0);
+        }
+        
+        // Copy constructor
+        VertexBuffer(VertexBuffer& src);
+        
+        // Copy assignment operator
+        VertexBuffer& operator=(VertexBuffer& src);
+        
+        // Move constructor
+        VertexBuffer(VertexBuffer&& src);
+        
+        // Move assignment operator
+        VertexBuffer& operator=(VertexBuffer&& src);
+        
+        // Bind the buffer
+        void bind() const;
+        
+        // Unbind the buffer
+        void unbind() const;
+        
+        // Set the data to an arbitrary vector
+        template<typename T>
+        void setData(std::vector<T> p_newData)
+        {
+            bind();
+            glBufferData(GL_ARRAY_BUFFER, p_data.size() * sizeof(T), p_data.data(), GL_STATIC_DRAW);
+            unbind();
+        }
+        
+    private:
+        unsigned int m_handle;
+    };
+}
+
+#endif /* CD895C87_5211_435E_820F_54EDC3B94158 */
