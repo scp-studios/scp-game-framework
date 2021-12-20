de9b93c9806b0528a3fc755e8b8fdb85262eaf6f
Liangyue "Yue" Zhao <Hello56721@github.com>
2021-12-20 11:47:03 -0500

    Implemented the Vertex Buffer class.

---
+++ src/graphics/opengl/VertexBuffer.cpp
@@ -0,0 +1,82 @@
+#include <algorithm>
+
+#include <scp/graphics/opengl/VertexBuffer.hpp>
+
+using scp::graphics::opengl::VertexBuffer;
+
+VertexBuffer::VertexBuffer(uint32_t p_size)
+{
+    glGenBuffers(1, &m_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, m_handle);
+    glBufferData(GL_ARRAY_BUFFER, p_size, nullptr, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+VertexBuffer::VertexBuffer(VertexBuffer& src)
+{
+    glBindBuffer(GL_COPY_READ_BUFFER, src.m_handle);
+    
+    glGenBuffers(1, &m_handle);
+    glBindBuffer(GL_ARRAY_BUFFER, m_handle);
+    
+    int32_t sourceBufferSize = 0;
+    glGetBufferParameteriv(GL_COPY_READ_BUFFER, GL_BUFFER_SIZE, &sourceBufferSize);
+    glBufferData(GL_ARRAY_BUFFER, sourceBufferSize, nullptr, GL_STATIC_DRAW);
+    glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_ARRAY_BUFFER, 0, 0, sourceBufferSize);
+    
+    glBindBuffer(GL_COPY_READ_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+VertexBuffer& VertexBuffer::operator=(VertexBuffer& p_rhs)
+{
+    VertexBuffer newBuffer = p_rhs;
+    swap(*this, newBuffer);
+    return *this;
+}
+
+VertexBuffer::VertexBuffer(VertexBuffer&& src)
+{
+    moveFrom(src);
+}
+
+VertexBuffer& VertexBuffer::operator=(VertexBuffer&& p_rhs)
+{
+    destroy();
+    moveFrom(p_rhs);
+    return *this;
+}
+
+void VertexBuffer::bind() const
+{
+    glBindBuffer(GL_ARRAY_BUFFER, m_handle);
+}
+
+void VertexBuffer::unbind() const
+{
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void VertexBuffer::swap(VertexBuffer& p_a, VertexBuffer& p_b)
+{
+    std::swap(p_a.m_handle, p_b.m_handle);
+}
+
+VertexBuffer::~VertexBuffer()
+{
+    destroy();
+}
+
+void VertexBuffer::moveFrom(VertexBuffer& src)
+{
+    m_handle = src.m_handle;
+    
+    // Reset the source values, because ownership has been moved!
+    src.m_handle = 0;
+}
+
+void VertexBuffer::destroy()
+{
+    glDeleteBuffers(1, &m_handle);
+}
