2295192ae8a2b919801e1d82ecee90e3ee9cfc45
Liangyue "Yue" Zhao <Hello56721@github.com>
2021-12-23 11:56:46 -0500

    Integrated stb_image into the project.

--- include/scp/utils/pch.hpp
+++ include/scp/utils/pch.hpp
@@ -1,8 +1,12 @@
 #ifndef BA5ED3DD_D401_4AE3_B1E2_E070E9ADF1DF
 #define BA5ED3DD_D401_4AE3_B1E2_E070E9ADF1DF
 
+#ifdef __cplusplus
+
 #include <string_view>
 #include <string>
 #include <iostream>
+
+#endif
 
 #endif /* BA5ED3DD_D401_4AE3_B1E2_E070E9ADF1DF */
