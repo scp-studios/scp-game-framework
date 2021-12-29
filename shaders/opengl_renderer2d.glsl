a4b5f210d219a6f417ddbc9d970c218fa94bc111
Liangyue "Yue" Zhao <Hello56721@github.com>
2021-12-29 17:13:29 -0500

    Changed the shader to suit the new renderer.

--- shaders/opengl_renderer2d.glsl
+++ shaders/opengl_renderer2d.glsl
@@ -1,19 +1,33 @@
 //#@ vertex
-#version 330 core
+#version 430 core
 layout (location = 0) in vec2 aPosition;
 layout (location = 1) in vec2 aUV;
-layout (location = 2) in uint spriteID;
+layout (location = 2) in vec4 aColor;
+layout (location = 3) in float aTexture;
+
+out vec4 color;
+out float myTexture;
+out vec2 uv;
 
 void main()
 {
     gl_Position = vec4(aPosition, 0.0, 1.0);
+    color = aColor;
+    myTexture = aTexture;
+    uv = aUV;
 }
 
 //#@ fragment
-#version 330 core
-out vec4 color;
+#version 430 core
+out vec4 outColor;
+
+uniform sampler2D textures[32];
+
+in vec4 color;
+in float myTexture;
+in vec2 uv;
 
 void main()
 {
-    color = vec4(0.0, 1.0, 0.0, 1.0);
+    outColor = color + texture(textures[int(myTexture)], uv);
 }
