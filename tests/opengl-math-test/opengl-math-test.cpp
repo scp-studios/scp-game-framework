2b88c3188d5179cbe4d67da41ae8e902dfefdc86
Liangyue "Yue" Zhao <Hello56721@github.com>
2022-01-20 17:20:10 -0500

    Tested the Math classes. They generally work.

--- tests/opengl-math-test/opengl-math-test.cpp
+++ tests/opengl-math-test/opengl-math-test.cpp
@@ -3,33 +3,109 @@
 #include <iostream>
 #include <scp/Window.hpp>
 #include <scp/Scene.hpp>
-#include <scp/graphics/Renderer2D.hpp>
-#include <scp/graphics/API.hpp>
+#include <scp/graphics/opengl.hpp>
+#include <scp/graphics/opengl/VertexArray.hpp>
+#include <scp/graphics/opengl/VertexBuffer.hpp>
+#include <scp/graphics/opengl/Shader.hpp>
+#include <scp/graphics/VertexLayout.hpp>
+#include <scp/graphics/opengl/ElementBuffer.hpp>
+#include <scp/graphics/opengl.hpp>
+#include <scp/utils/file-utils.hpp>
+#include <scp/math/Matrix4.hpp>
+#include <scp/math/Vector3.hpp>
+#include <scp/math.hpp>
+#include <scp/math/transformations.hpp>
+#include <scp/Input.hpp>
 
 using scp::Window;
 using scp::Scene;
-using scp::graphics::Renderer2D;
-using scp::graphics::API;
+using scp::graphics::opengl::Shader;
+using scp::graphics::opengl::VertexArray;
+using scp::graphics::opengl::VertexBuffer;
+using scp::graphics::VertexLayout;
+using scp::graphics::opengl::ElementBuffer;
+
+namespace math = scp::math;
+
+static float deltaTime = 0.0;
 
 class MyScene: public Scene
 {
 public:
-    MyScene(): renderer(API::OpenGL, "")
+    MyScene(): model(1.0f), Input(scp::Input::getInstance())
     {
+        std::vector<float> vertices = {
+             0.1f,  0.1f,
+             0.1f, -0.1f,
+            -0.1f, -0.1f,
+            -0.1f,  0.1f
+        };
         
+        indices = {
+            0, 1, 2,
+            0, 3, 2
+        };
+        
+        vertexBuffer = VertexBuffer(vertices);
+        
+        elementBuffer = ElementBuffer(indices);
+        
+        // NVIDIA's drivers demands that we bind vertex arrays before element
+        // buffers for some reasons.
+        vertexArray.bind();
+        elementBuffer.bind();
+        
+        VertexLayout vertexLayout = {};
+        vertexLayout.type = VertexLayout::Type::Float;
+        vertexLayout.size = 2;
+        vertexLayout.stride = 2 * sizeof(float);
+        vertexLayout.offset = 0;
+        
+        vertexArray.addAttribute(0, vertexBuffer, vertexLayout);
+        
+        std::string shaderSource = scp::utils::file_utils::loadAsString("../shaders/opengl-math-test.glsl");
+        shader = Shader(shaderSource, "../shaders/opengl-math-test.glsl");
     }
     
     void onUpdate() override
     {
-        
+        if (Input.isKeyDown(GLFW_KEY_RIGHT))
+        {
+            model = math::transformations::translate(model, scp::math::Vector3(0.5f * deltaTime, 0.0f, 0.0f));
+        }
+        if (Input.isKeyDown(GLFW_KEY_LEFT))
+        {
+            model = math::transformations::translate(model, scp::math::Vector3(-0.5f * deltaTime, 0.0f, 0.0f));
+        }
+        if (Input.isKeyDown(GLFW_KEY_UP))
+        {
+            model = math::transformations::translate(model, scp::math::Vector3(0.0f, 0.5f * deltaTime, 0.0f));
+        }
+        if (Input.isKeyDown(GLFW_KEY_DOWN))
+        {
+            model = math::transformations::translate(model, scp::math::Vector3(0.0f, -0.5f * deltaTime, 0.0f));
+        }
     }
     
     void render() override
     {
+        glClear(GL_COLOR_BUFFER_BIT);
         
+        shader.use();
+        
+        shader.setUniform("model", model);
+        
+        vertexArray.bind();
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, 0);
     }
 private:
-    Renderer2D renderer;
+    VertexArray vertexArray;
+    VertexBuffer vertexBuffer;
+    ElementBuffer elementBuffer;
+    std::vector<uint8_t> indices;
+    Shader shader;
+    scp::math::Matrix4<float> model;
+    scp::Input& Input;
 };
 
 class Application
@@ -42,13 +118,20 @@ public:
     
     void mainLoop()
     {
+        window.show();
+        
         while (window.isOpen())
         {
+            double startTime = glfwGetTime();
+            
             Scene::updateActive();
             
             Scene::renderActive();
             
             window.update();
+            
+            double endTime = glfwGetTime();
+            deltaTime = endTime - startTime;
         }
     }
     
