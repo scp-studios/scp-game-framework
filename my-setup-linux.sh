8381d6da4ca034114754aeb889aeaf568b79803a
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-01-26 17:02:14 -0500

    Made the configuration to be debug.

--- my-setup-linux.sh
+++ my-setup-linux.sh
@@ -1,5 +1,5 @@
-cmake -S deps/glfw -B deps/glfw/build -G Ninja -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D GLFW_BUILD_TESTS=false -D GLFW_BUILD_EXAMPLES=false
+cmake -S deps/glfw -B deps/glfw/build -G Ninja -D CMAKE_C_COMPILER=clang -D CMAKE_BUILD_TYPE=Debug -D CMAKE_CXX_COMPILER=clang++ -D GLFW_BUILD_TESTS=false -D GLFW_BUILD_EXAMPLES=false
 cmake --build deps/glfw/build
 
-cmake -S . -B build -G Ninja -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++
+cmake -S . -B build -G Ninja -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_BUILD_TYPE=Debug
 cmake --build build
