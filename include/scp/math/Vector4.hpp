f27930c043bde0afc600c7c7458ca1668d9363eb
Hello56721 <Hello56721@github.com>
2022-02-01 13:19:14 -0500

    Allowed converting from Vector2 to Vector4.

--- include/scp/math/Vector4.hpp
+++ include/scp/math/Vector4.hpp
@@ -1,6 +1,8 @@
 #ifndef DB62E672_4026_45F9_BCDD_D5FB31A75EB5
 #define DB62E672_4026_45F9_BCDD_D5FB31A75EB5
 
+#include "Vector2.hpp"
+
 namespace scp::math
 {
     template <typename T>
@@ -29,6 +31,11 @@ namespace scp::math
         }
         
         Vector4(T p_x, T p_y, T p_z, T p_w): x(p_x), y(p_y), z(p_z), w(p_w)
+        {
+            
+        }
+        
+        Vector4(const Vector2<T>& src): x(src.x), y(src.y), z(0), w(0)
         {
             
         }
