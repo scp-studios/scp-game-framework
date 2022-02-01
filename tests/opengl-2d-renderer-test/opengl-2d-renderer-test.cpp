94a319e4338227f0df9151290edf342ca765322e
Hello56721 <Hello56721@github.com>
2022-02-01 16:29:55 -0500

    Tested the Sprite renderer. It works.

--- tests/opengl-2d-renderer-test/opengl-2d-renderer-test.cpp
+++ tests/opengl-2d-renderer-test/opengl-2d-renderer-test.cpp
@@ -6,10 +6,13 @@
 
 #include <scp/Window.hpp>
 #include <scp/Scene.hpp>
+#include <scp/Input.hpp>
+#include <scp/math.hpp>
 
 #include <scp/graphics/opengl.hpp>
 #include <scp/graphics/Renderer2D.hpp>
 #include <scp/graphics/API.hpp>
+#include <scp/graphics/Sprite.hpp>
 
 #include <scp/utils/file-utils.hpp>
 
@@ -17,6 +20,10 @@ using scp::Window;
 using scp::Scene;
 
 using scp::graphics::Renderer2D;
+using scp::graphics::Sprite;
+
+using scp::math::Vector4;
+using scp::math::Vector2;
 
 namespace opengl = scp::graphics::opengl;
 namespace fileutils = scp::utils::file_utils;
@@ -24,16 +31,56 @@ namespace fileutils = scp::utils::file_utils;
 class MyScene: public Scene
 {
 public:
-    MyScene(): renderer(scp::graphics::API::OpenGL, fileutils::loadAsString("../shaders/opengl_renderer2d.glsl"))
+    MyScene(): renderer(scp::graphics::API::OpenGL, fileutils::loadAsString("../shaders/opengl_renderer2d.glsl")),
+               Input(scp::Input::getInstance())
     {
         renderer.addTexture("../textures/TestTexture.png");
         
         opengl::enable2DBlending();
+        
+        thing1.baseColor = Vector4(0.0f);
+        thing1.texture = 0;
+        thing1.uv[0] = Vector2(1.0f, 0.0f);
+        thing1.uv[1] = Vector2(1.0f, 1.0f);
+        thing1.uv[2] = Vector2(0.0f, 1.0f);
+        thing1.uv[3] = Vector2(0.0f, 0.0f);
+        thing1.scale = Vector2(128.0f, 128.0f);
+        thing1.position = Vector2(800.0f, 200.0f);
+        thing1.applyTransforms();
+        
+        thing2.baseColor = Vector4(0.0f, 1.0f, 0.0f, 0.5f);
+        thing2.texture = -1;
+        thing2.uv[0] = Vector2(0.0f, 0.0f);
+        thing2.uv[1] = Vector2(0.0f, 0.0f);
+        thing2.uv[2] = Vector2(0.0f, 0.0f);
+        thing2.uv[3] = Vector2(0.0f, 0.0f);
+        thing2.scale = Vector2(200.0f, 200.0f);
+        thing2.position = Vector2(400.0f, 300.0f);
+        thing2.applyTransforms();
     }
     
     void onUpdate() override
     {
+        if (Input.isKeyDown(GLFW_KEY_UP))
+        {
+            thing1.position.y -= 2.5f;
+        }
+        if (Input.isKeyDown(GLFW_KEY_DOWN))
+        {
+            thing1.position.y += 2.5f;
+        }
         
+        if (Input.isKeyDown(GLFW_KEY_W))
+        {
+            thing2.position.y -= 2.5f;
+        }
+        if (Input.isKeyDown(GLFW_KEY_S))
+        {
+            thing2.position.y += 2.5f;
+        }
+        
+        thing1.applyTransforms();
+        thing2.applyTransforms();
     }
     
     void render() override
@@ -41,8 +88,11 @@ public:
         renderer.begin();
         
         //renderer.drawTexturedQuad(0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1);
-        renderer.drawTexturedQuad(128.0f, 128.0f, 800.0f, 200.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0);
-        renderer.drawSolidColoredQuad(200.0f, 200.0f, 400.0f, 300.0f, 0.0f, 1.0f, 0.0f, 0.5f);
+        //renderer.drawTexturedQuad(128.0f, 128.0f, 800.0f, 200.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0);
+        //renderer.drawSolidColoredQuad(200.0f, 200.0f, 400.0f, 300.0f, 0.0f, 1.0f, 0.0f, 0.5f);
+        
+        renderer.drawSprite(thing2);
+        renderer.drawSprite(thing1);
         
         renderer.end();
     }
@@ -54,6 +104,11 @@ public:
     
 private:
     Renderer2D renderer;
+    
+    Sprite thing1;
+    Sprite thing2;
+    
+    scp::Input& Input;
 };
 
 class Application
