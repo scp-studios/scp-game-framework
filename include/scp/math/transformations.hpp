33aba6ae5b96a0281be1244593df0bf5af3d4e92
Hello56721 <Hello56721@github.com>
2022-02-01 14:26:10 -0500

    Added support for Vector2 transformations.

--- include/scp/math/transformations.hpp
+++ include/scp/math/transformations.hpp
@@ -1,8 +1,7 @@
 #ifndef B75DB157_5D6D_4EAF_9D64_7CD1F1307D4F
 #define B75DB157_5D6D_4EAF_9D64_7CD1F1307D4F
 
-#include "Matrix4.hpp"
-#include "Vector3.hpp"
+#include <scp/math.hpp>
 
 namespace scp::math
 {
@@ -15,7 +14,16 @@ namespace scp::math
             scaleMatrix.m_data[0][0] = v.x;
             scaleMatrix.m_data[1][1] = v.y;
             scaleMatrix.m_data[2][2] = v.z;
-            scaleMatrix.m_data[3][3] = 1;
+            
+            return scaleMatrix * m;
+        }
+        
+        template<typename T>
+        Matrix4<T> scale(Matrix4<T> m, Vector2<T> v)
+        {
+            Matrix4<T> scaleMatrix(1.0);
+            scaleMatrix.m_data[0][0] = v.x;
+            scaleMatrix.m_data[1][1] = v.y;
             
             return scaleMatrix * m;
         }
@@ -27,7 +35,16 @@ namespace scp::math
             translateMatrix.m_data[0][3] = v.x;
             translateMatrix.m_data[1][3] = v.y;
             translateMatrix.m_data[2][3] = v.z;
-            translateMatrix.m_data[3][3] = 1;
+            
+            return translateMatrix * m;
+        }
+        
+        template<typename T>
+        Matrix4<T> translate(Matrix4<T> m, Vector2<T> v)
+        {
+            Matrix4<T> translateMatrix(1.0);
+            translateMatrix.m_data[0][3] = v.x;
+            translateMatrix.m_data[1][3] = v.y;
             
             return translateMatrix * m;
         }
