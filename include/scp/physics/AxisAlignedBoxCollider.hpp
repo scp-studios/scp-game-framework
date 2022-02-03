b6ea2e866459d30bc40856c36494ce92b2bd81ed
Hello56721 <Hello56721@github.com>
2022-02-03 16:28:56 -0500

    Started on the AxisAlignedBoxCollider class.

---
+++ include/scp/physics/AxisAlignedBoxCollider.hpp
@@ -0,0 +1,28 @@
+#ifndef AE069409_165F_4668_B9FE_27666A88ACE4
+#define AE069409_165F_4668_B9FE_27666A88ACE4
+
+namespace scp::physics
+{
+    class AxisAlignedBoxCollider
+    {
+    public:
+        AxisAlignedBoxCollider(
+            uint16_t right = 100, 
+            uint16_t left = 0, 
+            uint16_t top = 0, 
+            uint16_t bottom = 100
+        );
+        
+        bool hasCollidedWith(const AxisAlignedBoxCollider& other) const;
+        
+        void move(uint16_t x, uint16_t y);
+        
+    private:
+        uint16_t right;
+        uint16_t left;
+        uint16_t top;
+        uint16_t bottom;
+    };
+}
+
+#endif /* AE069409_165F_4668_B9FE_27666A88ACE4 */
