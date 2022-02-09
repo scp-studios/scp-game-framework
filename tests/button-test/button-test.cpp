288118ff8a779467f65f46dfb4854802e020bbdb
Hello56721 <Hello56721@github.com>
2022-02-09 16:52:24 -0500

    Tested the button class. It works.

---
+++ tests/button-test/button-test.cpp
@@ -0,0 +1,174 @@
+#include <stdlib.h>
+
+//#ifdef DO_NOT_DEFINE_THIS_MACRO_OR_ELSE_I_WILL_COME_OVER_TO_YOUR_HOUSE_AND_BEAT_YOU_WITH_A_BAT
+
+#include <glad/glad.h>
+
+#include <scp/Window.hpp>
+#include <scp/Scene.hpp>
+#include <scp/Input.hpp>
+#include <scp/math.hpp>
+
+#include <scp/graphics/opengl.hpp>
+#include <scp/graphics/Renderer2D.hpp>
+#include <scp/graphics/API.hpp>
+#include <scp/graphics/Sprite.hpp>
+
+#include <scp/utils/file-utils.hpp>
+
+#include <scp/ui/Button.hpp>
+
+using scp::Window;
+using scp::Scene;
+
+using scp::graphics::Renderer2D;
+using scp::graphics::Sprite;
+
+using scp::math::Vector4;
+using scp::math::Vector2;
+
+using scp::ui::Button;
+
+namespace opengl = scp::graphics::opengl;
+namespace fileutils = scp::utils::file_utils;
+
+class MyScene: public Scene
+{
+public:
+    MyScene(): renderer(scp::graphics::API::OpenGL, fileutils::loadAsString("../shaders/opengl_renderer2d.glsl")),
+               Input(scp::Input::getInstance()),
+               button(400.0 + 200.0, 400.0, 300.0, 300.0 + 200.0)
+    {
+        renderer.addTexture("../textures/TestTexture.png");
+        
+        opengl::enable2DBlending();
+        
+        thing1.baseColor = Vector4(0.0f);
+        thing1.texture = 0;
+        thing1.uv[0] = Vector2(1.0f, 0.0f);
+        thing1.uv[1] = Vector2(1.0f, 1.0f);
+        thing1.uv[2] = Vector2(0.0f, 1.0f);
+        thing1.uv[3] = Vector2(0.0f, 0.0f);
+        thing1.scale = Vector2(128.0f, 128.0f);
+        thing1.position = Vector2(800.0f, 200.0f);
+        thing1.applyTransforms();
+        
+        thing2.baseColor = Vector4(0.0f, 1.0f, 0.0f, 0.5f);
+        thing2.texture = -1;
+        thing2.uv[0] = Vector2(0.0f, 0.0f);
+        thing2.uv[1] = Vector2(0.0f, 0.0f);
+        thing2.uv[2] = Vector2(0.0f, 0.0f);
+        thing2.uv[3] = Vector2(0.0f, 0.0f);
+        thing2.scale = Vector2(200.0f, 200.0f);
+        thing2.position = Vector2(400.0f, 300.0f);
+        thing2.applyTransforms();
+    }
+    
+    void onUpdate(double deltaTime) override
+    {
+        if (button.isClicked())
+        {
+            thing2.baseColor = Vector4(0.0f, 1.0f, 1.0f, 1.0f);
+        }
+        else if (button.isHovering())
+        {
+            thing2.baseColor = Vector4(1.0f, 1.0f, 0.0f, 1.0f);
+        }
+        else
+        {
+            thing2.baseColor = Vector4(0.0f, 1.0f, 0.0f, 0.5f);
+        }
+    }
+    
+    void render() override
+    {
+        renderer.begin();
+        
+        //renderer.drawTexturedQuad(0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1);
+        //renderer.drawTexturedQuad(128.0f, 128.0f, 800.0f, 200.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0);
+        //renderer.drawSolidColoredQuad(200.0f, 200.0f, 400.0f, 300.0f, 0.0f, 1.0f, 0.0f, 0.5f);
+        
+        renderer.drawSprite(thing2);
+        renderer.drawSprite(thing1);
+        
+        renderer.end();
+    }
+    
+    ~MyScene()
+    {
+        
+    }
+    
+private:
+    Renderer2D renderer;
+    
+    Sprite thing1;
+    Sprite thing2;
+    
+    Button button;
+    
+    scp::Input& Input;
+};
+
+class Application
+{
+public:
+    Application(): window(Window::getInstance())
+    {
+        scp::graphics::opengl::enableContextDebugging();
+        
+        Scene::setActive<MyScene>();
+        
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        
+        lastFrame = 0.0;
+        
+        window.show();
+        
+        while (window.isOpen())
+        {
+            double currentFrame = glfwGetTime();
+            double deltaTime = currentFrame - lastFrame;
+            lastFrame = currentFrame;
+            
+            Scene::updateActive(deltaTime);
+            
+            glClear(GL_COLOR_BUFFER_BIT);
+            
+            Scene::renderActive();
+            
+            window.update();
+        }
+    }
+    
+    ~Application()
+    {
+        
+    }
+private:
+    Window& window;
+    
+    // The time of the last frame.
+    double lastFrame;
+};
+
+//#endif
+
+int main()
+{
+    //#ifdef DO_NOT_DEFINE_THIS_MACRO_OR_ELSE_I_WILL_COME_OVER_TO_YOUR_HOUSE_AND_BEAT_YOU_WITH_A_BAT
+    try
+    {
+        Application app;
+    }
+    catch (std::runtime_error& e)
+    {
+        std::cerr << "[FATAL ERROR]: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    //#endif
+    
+    //std::cout << scp::utils::string_utils::startsWith("Hello World!", "Hello");
+    
+    return EXIT_SUCCESS;
+}
