7d0a2cf12885067d9e63c86c09619af1b281b953
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-02-14 18:09:00 -0500

    Made the linux version shared libraries by default.

--- setup-linux.sh
+++ setup-linux.sh
@@ -2,5 +2,5 @@
 cmake -S deps/glfw -B deps/glfw/build -D GLFW_BUILD_TESTS=false -D GLFW_BUILD_EXAMPLES=false
 cmake --build deps/glfw/build
 
-cmake -S . -B build
+cmake -S . -B build -D BUILD_SHARED_LIBS=true
 cmake --build build
